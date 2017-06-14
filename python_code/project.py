d49b8b4a9d15b0c38ad37750bb10c3cf8d144621
balilarder <balilarder@yahoo.com.tw>
2017-06-14 20:07:26 +0800

    first commit

---
+++ python_code/project.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def reading_file(fn):
+    # find the file path, read each row, return
+    path = ""
+    if fn == "Kaohsiung":
+        path = 
+    elif fn == 
+        
+
+
+
+
+def main():
+    source = ['Kaohsiung', 'Tainan', 'students', 'tumblr', 'facebook']
+    data = reading_file('Kaohsiung')       # list for input data
+
+
+if __name__ == '__main__':
+    main()
